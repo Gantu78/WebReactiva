258d58d07c951b1a53d71bbd75e596c9c974138f
juanfe <juanfeliperzmcs@gmail.com>
2025-11-08 11:32:17 -0500

    init

---
+++ src/main/java/edu/javeriana/reactiva/tallerreactiva/TallerReactivaApplication.java
@@ -0,0 +1,13 @@
+package edu.javeriana.reactiva.tallerreactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerReactivaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerReactivaApplication.class, args);
+    }
+
+}
